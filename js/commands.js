cb352a0a8200b096487641d52384a0c74b2379fc
sean <seanluse41@gmail.com>
2024-11-12 17:09:05 +0900

    maybe

--- js/commands.js
+++ js/commands.js
@@ -1,32 +1,34 @@
+import executors from "./executors.js";
+
 export default [
   {
     name: ["search", "s"],
     description: "Searches Google for the given query",
-    execute: executor.search,
+    execute: executors.search,
   },
   {
     name: ["ls"],
     description: "Lists available shortcuts",
-    execute: executor.ls,
+    execute: executors.ls,
   },
   {
     name: ["help"],
     description: "Lists available commands",
-    execute: executor.help,
+    execute: executors.help,
   },
   {
     name: ["clear"],
     description: "Clears the output history",
-    execute: executor.clear,
+    execute: executors.clear,
   },
   {
     name: ["weather"],
     description: "Displays the weather forecast",
-    execute: executor.weather,
+    execute: executors.weather,
   },
   {
-    name: ["*"],  // Catch-all command
+    name: ["*"], // Catch-all command
     description: "Search Google (type anything)",
-    execute: (args, fullCommand) => executor.search([fullCommand, ...args]),
+    execute: (args, fullCommand) => executors.search([fullCommand, ...args]),
   }
 ];
